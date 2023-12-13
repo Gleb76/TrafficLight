671c430a8cd94fb5012fda1c1942c150355f66bd
Gleb Klyga <klyga90@bk.ru>
2023-12-13 12:50:24 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Глеб Клыга on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
